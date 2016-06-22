f2515b8554e27dcd91eef2d84a87d61440cf6b12
Alex Macdonald-Smith <amac@apexenergygroup.com>
2016-06-21 21:50:40 -0400

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,6 +4,7 @@
     <h3>About Us</h3>
     <hr>
     <div class="well well-lg">
+        <p>test</p>
         <p style="color: #222222; font-size: larger;">this paragraph is a short brief introduction of our relationship</p>
     </div>
 @stop
